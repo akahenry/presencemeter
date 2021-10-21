c8e0ed07eb7d7370455ea20a8df10e120fcf30da
MSWLandi <fakepsnmarcos@gmail.com>
2021-10-20 22:15:04 -0300

    add schedule to class

--- AddClass.tsx
+++ AddClass.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Alert, Text, View, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { Appbar, Avatar, TextInput, FAB, Title } from 'react-native-paper';
+import { Appbar, Avatar, TextInput, FAB, Button, Dialog, Portal, List } from 'react-native-paper';
+import DropDown from "react-native-paper-dropdown";
 
 import * as cls from './class';
 
@@ -59,6 +60,71 @@ const FormLocation = (props) => {
     );
 };
 
+const FormTime = (props) => {
+    const [showDropDown, setShowDropDown] = React.useState(false);
+    const [day, setDay] = React.useState("");
+    const [hours, setHours] = React.useState("");
+    const dayList = [
+        {
+        label: "Segundas",
+        value: "Segundas",
+        },
+        {
+        label: "Terças",
+        value: "Terças",
+        },
+        {
+        label: "Quartas",
+        value: "Quartas",
+        },
+        {
+        label: "Quintas",
+        value: "Quintas",
+        },
+        {
+        label: "Sextas",
+        value: "Sextas",
+        },
+        {
+        label: "Sábados",
+        value: "Sábados",
+        },
+        {
+        label: "Domingos",
+        value: "Domingos",
+        },
+    ];
+
+    return (
+        <Portal>
+            <Dialog visible={props.visible} onDismiss={() => props.setVisible(false)}>
+                <Dialog.Title>Escolha um horário</Dialog.Title>
+                <Dialog.Content>
+                    <DropDown
+                        label={"Dia da Semana"}
+                        mode={"outlined"}
+                        visible={showDropDown}
+                        showDropDown={() => setShowDropDown(true)}
+                        onDismiss={() => setShowDropDown(false)}
+                        value={day}
+                        setValue={setDay}
+                        list={dayList}
+                    />
+                    <TextInput
+                        mode='outlined'
+                        value={hours}
+                        onChangeText={text => setHours(text)}
+                        label='Horário'
+                    />
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={() => {props.setVisible(false); props.onAccept({day, hours}); }}>Adicionar</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </Portal>
+    );
+}
+
 const AddClass = ({ route, navigation }) => {
     const [name, setName] = React.useState('');
     const [maxMisses, setMaxMisses] = React.useState(0);
@@ -68,24 +134,38 @@ const AddClass = ({ route, navigation }) => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     });
+    const [timeFormVisible, setTimeFormVisible] = React.useState(false);
+    const [schedule, setSchedule] = React.useState([])
 
     return (
         <View style={styles.mainView}>
             <Appbar.Header style={styles.appbar} statusBarHeight={5}>
                 <Avatar.Image style={styles.avatar} size={50} source={require('./assets/avatar.jpeg')} />
                 <Appbar.Content title="Presencemeter" subtitle="Aula" />
-                <Appbar.Action style={styles.cog} icon="cog" onPress={() => console.log("cog clicked")} />
+                <Appbar.Action style={styles.cog} icon="cog" onPress={() => console.log("cog clicked")}/>
             </Appbar.Header>
-            <View style={styles.page}>
+            <ScrollView style={styles.page}>
                 <FormText label="Nome" style={styles.formText} onChange={setName} />
                 <FormLocation label='Localização' style={styles.formText} onChange={setRegion} />
                 <FormNumber label='Faltas máximas' style={[styles.formText, styles.faltasMaximas]} onChange={setMaxMisses} />
+                <FormTime visible={timeFormVisible} onAccept={(returnedTime) => setSchedule(schedule.concat(returnedTime)) } setVisible={setTimeFormVisible} />
+                <List.Section>
+                    <Text style={styles.formLabel}>Horários</Text>
+                    {
+                        schedule.map((s, i) => {
+                            return (
+                            <List.Item style={styles.scheduleListItem} key={i} title={`${s.day} - ${s.hours}`} left={() => <List.Icon icon="calendar-check" />} />
+                            );
+                        })
+                    }
+                </List.Section>
+                <Button icon="calendar-plus" mode="contained" onPress={() => setTimeFormVisible(true)}>Adicionar Horário</Button>
                 <FAB
                     style={styles.fab}
-                    label='Salvar'
-                    onPress={() => { if (name.length > 0) { route.params.onSubmit(new cls.Class(name, [], region != null, 0, maxMisses, region)); navigation.goBack(); } }}
-                />
-            </View>
+                    label="Salvar"
+                    onPress={() => { if (name.length > 0) { route.params.onSubmit(new cls.Class(name, [], region != null, 0, maxMisses, region)); navigation.goBack(); } } }
+                    icon={undefined}/>
+            </ScrollView>
         </View>
     );
 }
@@ -121,7 +201,11 @@ const styles = StyleSheet.create({
         color: '#000000'
     },
     fab: {
-        backgroundColor: '#4b963f'
+        backgroundColor: '#4b963f',
+        marginTop: 15,
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 30
     },
     map: {
         ...StyleSheet.absoluteFillObject,
@@ -130,6 +214,11 @@ const styles = StyleSheet.create({
     },
     faltasMaximas: {
         marginTop: 150
+    },
+    scheduleListItem: {
+        marginBottom: -12,
+        marginTop: -12,
+        marginLeft: -10,
     }
 
 });
